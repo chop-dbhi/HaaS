139eb3594077baf0d4fb9b691659becdac7b01b3
James Swanick <swanijam@gmail.com>
2015-04-23 14:08:04 -0400

    Add ability to find containers built in previous requests
    Signed-off-by: James Swanick <swanijam@gmail.com>

--- site.py
+++ site.py
@@ -1,24 +1,25 @@
 import os
 import subprocess
 import urllib
-import json
 import time
 import random
+from uuid import uuid4
 
 from flask import Flask, render_template, url_for, request, redirect, send_from_directory
 
-
 app = Flask(__name__)
 
-# Confirm that a provided file has an acceptable extension.
-# In this case, '.csv' is the only allowed extension.
-def allowed_file(filename):
-    return ('.' in filename) and (filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS)
+TEMP_DIR = 'tmp/'
+METADATA_FILE = 'metadata.csv'
+DATA_FILE = 'data.csv'
 
 # Retrieve the files uploaded by the user, and if they are valid,
 #  store them in a new generated subfolder of tmp/.
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
+    if request.method == 'GET':
+        return render_template('index.html')
+
     if request.method == 'POST':
         metadatafile = request.files['metadatafile']
         datafile = request.files['datafile']
@@ -26,64 +27,59 @@ def upload_file():
         if not metadatafile or not datafile:
             render_template('index.html', missing_fields=True)
 
-        if not metadatafile.filename.endswith('.csv') 
-           or not datafile.filename.endswith('.csv'):
+        if not metadatafile.filename.endswith('.csv') \
+          or not datafile.filename.endswith('.csv'):
             render_template('index.html', wrong_filetype=True)
 
         print "Saving files..."
-        app.config['UPLOAD_FOLDER'] = 'tmp/'+str(random.randint(10000,99999))+'/'
-        while os.path.exists(app.config['UPLOAD_FOLDER']):
-            app.config['UPLOAD_FOLDER'] = 'tmp/'+str(random.randint(10000,99999))+'/'
-           
-        os.mkdir(app.config['UPLOAD_FOLDER'])
-        metadatafile.save(
-            os.path.join(
-                app.config['UPLOAD_FOLDER'],
-                metadatafile.filename
-            )
-        )
-        datafile.save(
-            os.path.join(
-                app.config['UPLOAD_FOLDER'],
-                datafile.filename
-            )
-        )
-        return redirect(url_for(
-                            'uploaded_file', 
-                            datafilename = datafile.filename, 
-                            metadatafilename = metadatafile.filename
-                        ))
+        uniqueid = str(uuid4())
+        folder = os.path.join(TEMP_DIR, uniqueid)
+        os.mkdir(folder)
 
-    # If method is not 'POST' (i.e., the user has not uploaded files yet)
-    #  or if files are not accepted, load the form.
-    return render_template('index.html')
+        metadatafile.save(os.path.join(folder, METADATA_FILE))
+        datafile.save(os.path.join(folder, DATA_FILE))
+
+        return redirect(url_for('container', uuid=uniqueid))
+
 
 # Generate the Docker container from the uploaded files, 
 #  and redirect to the new Harvest instance.
-@app.route('/uploads/<datafilename>/<metadatafilename>')
-def uploaded_file(datafilename, metadatafilename):
-    print 'Files accepted. Generating Docker Container...' 
-    containerid = subprocess.check_output([
-                      'docker', 'run', '-d', '-P',
-                      '-e', ('DATA_FILE=' + datafilename), 
-                      '-e', ('METADATA_FILE=' + metadatafilename),
-                      '-v', (os.path.abspath(app.config['UPLOAD_FOLDER']) + ':/input'),
-                      'dbhi/redcap-harvest'
-                  ])
-    containerid = containerid[0:9]  # Get short container id for use in inspect
-    containerinfo = json.loads(subprocess.check_output(['docker','inspect',containerid]))
+@app.route('/containers/<uuid>')
+def container(uuid):
+    folder = os.path.join(TEMP_DIR, uuid)
+    cidf = os.path.join(folder, 'docker.cid')
+    containerid = ''
+    if not os.path.exists(cidf):
+        print 'Files accepted. Generating Docker Container...' 
+        containerid = subprocess.check_output([
+                          'docker', 'run', '-d', '-P',
+                          '-v', (folder+':/input'),
+                          'dbhi/redcap-harvest'
+                      ])
+        containerid = containerid.strip()  # Get short container id for use in inspect
+        with open(cidf, 'w') as f:
+            f.write(containerid)
+    else:
+        with open(cidf) as f:
+            containerid = f.read()
+
+    portnumber = json.loads(subprocess.check_output(
+                                              ['docker','port', containerid, '8000']
+                                         ))
    
     print 'Waiting for harvest to load...' 
-    harvestIP = 'http://'+subprocess.check_output(['boot2docker', 'ip']).strip()+':'+\
-                  containerinfo[0]['NetworkSettings']['Ports']['8000/tcp'][0]['HostPort']
-    while True:
+    harvestIP = 'http://' + subprocess.check_output(['boot2docker', 'ip']).strip()\
+                      + portnumber
+   
+    for i in range(30):
         try:
             urllib.urlopen(harvestIP).getcode()
             print 'Done.'
             return redirect(harvestIP) 
         except StandardError:
             time.sleep(1)
-    return None
+    return 'There was a problem generating the Harvest project. \
+              Pease make sure your files are valid and try again.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug=True)
